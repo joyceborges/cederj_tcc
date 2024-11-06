932f8298dd34dabff27e404afea2dd1118b6f066
Joyce Borges <jlizcborges@gmail.com>
2024-11-06 18:34:46 -0300

    Refato arquitetura: cria pasta models e projects. Adiciona argpass a main.py e inicia a construção de controlador

---
+++ models/base_model.py
@@ -0,0 +1,21 @@
+from abc import ABC, abstractmethod
+
+class BaseModel(ABC):
+    '''
+    Base class for all models
+    '''
+
+    def __init__(self, model_name, model_type, model_path, model_params):
+        pass
+
+    @abstractmethod
+    def train(self, train_params):
+        pass
+
+    @abstractmethod
+    def test(self, test_params):
+        pass
+
+    @abstractmethod
+    def predict(self, predict_params):
+        pass
